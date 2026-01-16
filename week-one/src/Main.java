2a9b3ae42859b4de5ee14a1f890c7f09fbb00921
Wajahat <wajhi2004@gmail.com>
2026-01-16 18:25:38 +0500

    Week-one

--- week-one/src/Main.java
+++ week-one/src/Main.java
@@ -1,5 +1,4 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         Car c1 = new Car("Toyota", "Supra Celica", 510000);
@@ -9,6 +8,5 @@ public class Main {
 
         Student s1 = new Student("John Wick", 101);
         s1.displayInfo();
-
     }
 }
